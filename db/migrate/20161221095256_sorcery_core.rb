de08e774ccb4c1f83872e7f73bd7881947e05cbe
yaaaaashiki <yaaaaakishi@gmail.com>
2016-12-22 14:39:37 +0900

    clean up

--- db/migrate/20161221095256_sorcery_core.rb
+++ db/migrate/20161221095256_sorcery_core.rb
@@ -4,10 +4,8 @@ class SorceryCore < ActiveRecord::Migration
       t.string :email,            :null => false
       t.string :crypted_password
       t.string :salt
-
       t.timestamps
     end
-
     add_index :users, :email, unique: true
   end
 end
