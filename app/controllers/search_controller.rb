ae40a5998e57cdb2174db6a81a7650a90d4414f8
yaaaaashiki <yaaaaakishi@gmail.com>
2017-01-04 15:28:07 +0900

    update initialize.sh

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,12 @@
 class SearchController < ApplicationController
   # 開発環境のみログインなし
-  if Rails.env == 'development' then
+  if Rails.env == 'development'
     skip_before_filter :require_login
   end
 
   def index
-    if params['q'] then
-      @theses = Thesis.new.search(params['q'])
+    if params[:q] 
+      @theses = Thesis.new.search(params[:q])
     end
   end
 end
