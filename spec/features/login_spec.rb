593385dcf419e0770592c991c2e82b27d41a68c5
yaaaaashiki <yaaaaakishi@gmail.com>
2017-02-07 09:20:24 +0900

    add database cleaner

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,25 +1,21 @@
 require 'rails_helper'
 
 feature 'login page test' do
-  # let(:user) { create(:user) }
-  # let(:user) { User.first }
+  let(:user) { create(:user) }
 
   background do
     visit "/login"
   end
 
   scenario 'user data is correct' do 
-    skip '(T_T)' # userをcreateするたびに増える...
-# binding.pry
     expect(user.id).to eq(1) 
   end 
   
   scenario 'when login and data should insert user db' do
     # データベースに保存(データ数が増加)
     # seedでユーザデータをEscapism_testデータベースにいれたほうが良いかも
-    existedUser = create(:user)
 
-    fill_in  'username', with: 'user1'
+    fill_in  'username', with: user.username 
     fill_in  'password', with: 'password'
     click_on 'submit'
 
