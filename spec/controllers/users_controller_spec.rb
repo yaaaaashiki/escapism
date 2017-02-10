d4f6df5de62180e86f4b0bf1d6da226e6dd00f9a
Tatsuki Matsumoto <tatsukimatsumo@gmail.com>
2017-02-10 15:33:41 +0900

    temp

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
+# 書き途中www
 RSpec.describe UsersController, type: :controller do
 
   describe "GET #index" do
@@ -12,8 +13,55 @@ RSpec.describe UsersController, type: :controller do
     end
 
     it "renders the index templete" do
-      get :index
       expect(response).to render_template(:index)
+    end
+
+    it "assign true into @bookBack" do
+      expect(assigns(:bookBack)).to be true
+    end
+  end
+
+  describe "GET #new" do
+    before do 
+      get :new
+    end
+
+    context "exist Token in database" do
+      before do
+        Token.create()
+      end
+
+      it "returns http redirect" do
+        expect(response).to have_http_status(:redirect)
+      end
+
+      it "renders the new templete" do
+        expect(response).to redirect_to(search_url)
+      end
+    end
+
+    context "not exist Token in database" do
+      it "returns http redirect" do
+        expect(response).to have_http_status(:redirect)
+      end
+
+      it "renders the new templete" do
+        expect(response).to redirect_to(root_path)
+      end
+    end
+  end
+
+  describe "POST #create" do
+    before do 
+      post :create 
+    end
+
+    it "returns http success" do
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the create templete" do
+      expect(response).to render_template(:create)
     end
   end
 
