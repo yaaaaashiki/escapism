7d1377856add1884c7e8eb6b34ae6a750d0ee0f9
yaaaaashiki <yaaaaakishi@gmail.com>
2017-02-12 09:46:59 +0900

    update

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -6,7 +6,7 @@ class WordCount
 
   THESIS_ROOT_DIRECTORY = Rails.root.join('thesis_data')
    # こちらの配列に修造の名言を品詞分解したものを放り込んでいく。
-  words_and_count = [] 
+
   arraytext = [] 
   # csvファイルを読み込む。1行ずつ処理。
 
@@ -21,19 +21,25 @@ class WordCount
         nm = Natto::MeCab.new
         arraytext.each do |row|
           nm.parse(row) do |n|
-            s = n.surface 
-            words << s
+             words << n.surface  if (n.feature.match(/(固有名詞|名詞,一般)/)) and (n.surface.length > 1)
           end
         end
       end
     end
+    words_count = [] 
+
+    words.uniq.map do |word|  
+        words_count[words_count.size] = ["#{word}", "#{words.grep(word).count}"] if word
+    end
+ 
+    pp words_count.sort_by { |word_count| word_count[1].to_i }.reverse
+
   end
 
   private
     # pathにはURLも可
     def extractText(path)
       data = Yomu.new path
-
       rawText = data.text
       rawText.gsub(/\r\n|\n|\r/, "")
     end
