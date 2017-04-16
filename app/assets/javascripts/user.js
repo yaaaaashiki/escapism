0461f2f9f7ef82f23442fa2be8fab7151855f53a
yaaaaashiki <yaaaaakishi@gmail.com>
2017-04-16 10:23:17 +0900

    fix css name

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,13 +1,10 @@
 $(document).ready(function() {
   $("p").click(function(){
     $("p").css("color", "red")
-    $("div .col-jquery-left").css("background-image", "url(assets/kana2.jpg)")
-    $("div .col-jquery-right").css("background-image", "url(assets/kana2.jpg)")
-    $("div .col-jquery-left").css("background-size", "cover")
-    $("div .col-jquery-left").css("background-size", "contain")
-    $("div .col-jquery-right").css("background-size", "cover")
-    $("div .col-jquery-right").css("background-size", "contain")
-    $("div .image-left").fadeOut("slow")
-    $("div .image-right").fadeOut("slow")
+    $("div .before-image-left").fadeOut("slow")
+    $("div .before-image-right").fadeOut("slow")
+    $("div .after-image-left").fadeIn("slow")
+    $("div .after-image-right").fadeIn("slow")
+
   });
 });
