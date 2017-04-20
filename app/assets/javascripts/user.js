e4683db5c7e79cd48328d9aa20de16cc6f7af875
yaaaaashiki <yaaaaakishi@gmail.com>
2017-04-20 13:40:55 +0900

    wip

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,17 +1,27 @@
 $(document).ready(function() {
-  $("p").click(function(){
-    $("p").css("color", "red")
-    $("div .before-image-left").fadeOut("slow")
-    $("div .before-image-right").fadeOut("slow")
-    $("div .after-image-left").fadeIn("slow")
-    $("div .after-image-right").fadeIn("slow")
-  });
-//  $("p").toggle(
-//    function() {
-//  		 $("div .before-image-left").fadeOut("slow")
-//  	},
-//  	function() {
-//  		 $("div .after-image-left").fadeIn("slow")
-//  	}
-//  );
+//  $("p").click(function(){
+//    $("p").css("color", "red")
+//    $("div .before-image-left").fadeOut("slow")
+//    $("div .before-image-right").fadeOut("slow")
+//    $("div .after-image-left").fadeIn("slow")
+//    $("div .after-image-right").fadeIn("slow")
+//  });
+//  $("p").click(function(){
+//    $("p").toggle(
+//      function() {
+//    		 $("div .before-image-left").fadeOut("slow")
+//    	},
+//    	function() {
+//    		 $("div .after-image-left").fadeIn("slow")
+//    	}
+//    );
+//  });
+//
+  $("p").on('click', function(){
+    $("div .before-image-left").fadeOut(5000)
+    $("div .before-image-left").fadeIn(5000)
+});
+
+
+
 });
