b73fab989786201111cbbb883afb4dff31a21394
Tatsuki Matsumoto <tatsukimatsumo@gmail.com>
2017-04-30 17:50:39 +0900

    Remove unuse js read

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,6 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap-sprockets
-//= require jquery.turbolinks
 //= require jquery_ujs
-//= require turbolinks
-//= require app
-//= require_tree .
+//= require bootstrap-sprockets
+//= require common
