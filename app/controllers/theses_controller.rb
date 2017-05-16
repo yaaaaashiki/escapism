00e560a085df1048a442aa145ab0e56b4304ff5a
yaaaaashiki <yaaaaakishi@gmail.com>
2017-05-17 00:40:00 +0900

    remove search controller

--- app/controllers/theses_controller.rb
+++ app/controllers/theses_controller.rb
@@ -1,4 +1,38 @@
 class ThesesController < ApplicationController
+
+  THESIS_ROOT_DIRECTORY = Rails.root.join('thesis_data')
+  CLIENT = Elasticsearch::Client.new log: true
+  INDEX = 'thesis_development'
+  TYPE = 'thesis'
+
+  impressionist :actions=>[:index]
+
+  def index
+ #   @widget = Widget.find
+ #   impressionist(@widget, "message...") # 2nd argument is optional
+ #   @widget.impressionist_count(:filter=>:ip_address)
+
+    if params[:q]
+      response = search(params[:q])
+      thesisArray = []
+      response["hits"]["hits"].each do |t|
+        if params[:l]
+          thesis = Thesis.find_by(id: t["_id"], labo_id: params[:l])
+        else
+          thesis = Thesis.find(t["_id"])
+        end
+
+        if thesis
+          thesis = {thesis: thesis, body: t["_source"]["text"]}
+          thesisArray.push(thesis)
+        end
+      end
+      @thesisArray = Kaminari.paginate_array(thesisArray).page(params[:page]).per(4)
+    end
+
+    @labos = Labo.all
+  end
+
   def show
     @thesis = Thesis.find params[:id]
     @author = Author.find @thesis.author_id
@@ -8,4 +42,21 @@ class ThesesController < ApplicationController
     thesis = Thesis.find params[:id]
     send_file(thesis.url, disposition: :inline)
   end
+
+  private
+    def search(keyword = "")
+      response = CLIENT.search(index: INDEX, body: {
+        query: {
+          multi_match: {
+            query: keyword,
+            fields: ["text"]
+          }
+        },
+        sort: { _score: { order: "desc" } } # 検索結果の一致度の降順でソート
+        # Elasticsearchから返す検索結果の数をいじりたいときは以下を使用
+        # from: page * PAGE_SIZE,  # 返す検索結果の開始位置(0が最初)
+        # size: PAGE_SIZE   # 返す検索結果の数
+      })
+    end
+
 end
