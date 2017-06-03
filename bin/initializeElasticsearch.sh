d46a3321920caac616bf1559420da447df27f042
yaaaaashiki <yaaaaakishi@gmail.com>
2017-06-03 22:23:21 +0900

    Update initializeElasticsearch.sh

--- bin/initializeElasticsearch.sh
+++ bin/initializeElasticsearch.sh
@@ -1,16 +1,16 @@
 curl -XDELETE 'localhost:9200/thesis_development?pretty' &&
-  curl -XPUT 'localhost:9200/thesis_development?pretty' -d '{
-    "mappings" : {
-      "thesis" : {
-        "properties" : {
-         "text" : {
-           "type" : "string",
-            "analyzer" : "kuromoji"
-          }
+curl -XPUT 'localhost:9200/thesis_development?pretty' -d '{
+  "mappings" : {
+    "thesis" : {
+      "properties" : {
+        "text" : {
+          "type" : "string",
+          "analyzer" : "kuromoji"
         }
       }
     }
-  }' &&
+  }
+}' &&
 curl -XGET 'localhost:9200/thesis_development/_mapping/thesis?pretty' &&
 curl -XPUT 'localhost:9200/_settings' -d '{
   "index" : {
