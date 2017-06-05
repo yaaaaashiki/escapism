f98eb55c2655d6ec10933a892f994ff054d17c7d
masaya <mysubmail1@gmail.com>
2017-06-05 14:42:29 +0900

    implement carousel introduction

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -1,7 +1,7 @@
 window.onunload = function(){
   $('body').addClass('fade-in');
 };
-  
+
 $(window).on("unload",function(){
 });
 
@@ -11,12 +11,13 @@ $(window).on('load', function(){
 
 $(function() {
   $('a').on('click', function(e) {
-    $('body').addClass('fade-out');
-    url = $(this).attr('href');
-    setTimeout(function(){
-      window.location = url;
-    }, 800);
-
+    if(!$(this).hasclass('carousel-control')) {
+      $('body').addClass('fade-out');
+      url = $(this).attr('href');
+      setTimeout(function(){
+        window.location = url;
+      }, 800);
+    }
     return false;
   });
 
