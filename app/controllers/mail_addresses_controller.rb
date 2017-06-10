5f19aa83cbdf210fe9396644fa4ac26d78ffc7fd
yaaaaashiki <yaaaaakishi@gmail.com>
2017-06-10 16:00:38 +0900

    display error message in address input page

--- app/controllers/mail_addresses_controller.rb
+++ app/controllers/mail_addresses_controller.rb
@@ -2,13 +2,23 @@ class MailAddressesController < ApplicationController
   skip_before_action :require_login, only: [:new, :create]
   SUBJECT  = "論文検索システム Escapism です"
 
+  def index
+  end
+
   def new
     @mail = MailAddress.new
   end
 
   def create
-    send_email(params[:address][:name]) if MailAddress.create(address: params[:address][:name]).valid?
-    redirect_to root_path
+    @mail = MailAddress.new(address: params[:address][:name])
+    if @mail.invalid?
+      binding.pry
+      render :new
+    else
+      @mail.save
+      send_email(params[:address][:name])
+      redirect_to root_path
+    end
   end
 
   private
