793a31f2f0171818bfe3456bce7bb9b07abeccc1
yaaaaashiki <yaaaaakishi@gmail.com>
2017-06-16 23:35:37 +0900

    update search result and add access remitation rearound mailaddress

--- app/controllers/mail_addresses_controller.rb
+++ app/controllers/mail_addresses_controller.rb
@@ -1,5 +1,6 @@
 class MailAddressesController < ApplicationController
   skip_before_action :require_login, only: [:index, :new, :create]
+  before_action :exists_mail?, only: [:index]
   SUBJECT  = "【論文検索システム Escapism】 ユーザ登録ページのご案内"
 
   def index
@@ -23,5 +24,9 @@ class MailAddressesController < ApplicationController
 
     def send_email(address)
       Admin::InviteUserMailer.invite(address, SUBJECT).deliver
+    end
+
+    def exists_mail?
+      raise ActionController::RoutingError.new('here is only admin user page') unless Rails.application.routes.recognize_path(request.referrer)[:action] == "new"
     end
 end
