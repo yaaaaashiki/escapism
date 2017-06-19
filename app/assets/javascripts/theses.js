6c9d7da5faafe59d199c1d1aeb25ebe4df010fa0
yaaaaashiki <yaaaaakishi@gmail.com>
2017-06-19 21:24:34 +0900

    a few fix at regular expression

--- app/assets/javascripts/theses.js
+++ app/assets/javascripts/theses.js
@@ -1,6 +1,6 @@
 $(function() {
 
-  console.log(location.search.match(/&l=(.*?)(&|$)/)[1]);
+  console.log(location.search.match(/&l=(.*?)(&|&|$)/)[1]);
 
   $("[id^=l]").change(function() {
     $("[id^=l]").removeClass('checked');
