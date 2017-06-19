def37ed989c479ac76543089c47b86c75b2dbeed
yaaaaashiki <yaaaaakishi@gmail.com>
2017-06-19 13:06:52 +0900

    get radio button parameter

---
+++ app/assets/javascripts/theses.js
@@ -0,0 +1,11 @@
+$(function() {
+
+  console.log(location.search.match(/&l=(.*?)(&|$)/)[1]);
+
+  $("[id^=l]").change(function() {
+    $("[id^=l]").removeClass('checked');
+    $(this).addClass('checked');
+  });
+
+
+});
