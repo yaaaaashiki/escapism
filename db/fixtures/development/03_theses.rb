4ed4d7c1ed18b275ebda7ccadf6c20bb6c9b4c62
yaaaaashiki <yaaaaakishi@gmail.com>
2017-07-08 15:47:08 +0900

    rm comment out

--- db/fixtures/development/03_theses.rb
+++ db/fixtures/development/03_theses.rb
@@ -2,6 +2,6 @@ Thesis.__elasticsearch__.create_index! force: true
 Thesis.__elasticsearch__.refresh_index!
 
 ThesisImporter.upsert_all!(Thesis.LABO_2014_THESES)
-#ThesisImporter.upsert_all!(Thesis.LABO_2015_THESES)
-#ThesisImporter.upsert_all!(Thesis.LABO_2016_THESES)
+ThesisImporter.upsert_all!(Thesis.LABO_2015_THESES)
+ThesisImporter.upsert_all!(Thesis.LABO_2016_THESES)
 
