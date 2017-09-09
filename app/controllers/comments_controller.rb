591eaf00ddfc118fd69f2b47c98fb03c6697d3c5
yaaaaashiki <yaaaaakishi@gmail.com>
2017-09-09 09:26:14 +0900

    Add empty comment validation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,16 @@
 class CommentsController < ApplicationController
+  EMPTY = 0
+
   def create
     @thesis = Thesis.find(params[:thesis_id])
     if @thesis.nil?
       logger.error("Internal server error: CommentsController create action 3 lines: @theses is undefined")
       render_500
+    end
+
+    if empty_comment?(comment_params[:body])
+      redirect_to thesis_path(@thesis)
+      return
     end
     @comment = @thesis.comments.create(body: comment_params[:body], user_id: session[:user_id])
     redirect_to thesis_path(@thesis)
@@ -12,5 +19,9 @@ class CommentsController < ApplicationController
   private
     def comment_params
       params.require(:comment).permit(:body)
+    end
+
+    def empty_comment?(comment)
+      comment.length == EMPTY
     end
 end
