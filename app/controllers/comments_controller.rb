08633be91c0f6d69e4ddfcf22b1ff925967b96aa
yaaaaashiki <yaaaaakishi@gmail.com>
2017-09-09 20:02:48 +0900

    Implement multiple lines validation for theses comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,17 @@
 class CommentsController < ApplicationController
+  MAX_LINES = 10
+
 
   def create
     @thesis = Thesis.find(params[:thesis_id])
     if @thesis.nil?
       logger.error("Internal server error: CommentsController create action 3 lines: @theses is undefined")
       render_500
+    end
+
+    if multiple_lines?(trim_multiple_newlines(comment_params[:body]))
+      redirect_to thesis_path(@thesis)
+      return
     end
 
     @comment = @thesis.comments.create(body: trim_multiple_newlines(comment_params[:body]), user_id: session[:user_id])
@@ -18,5 +25,12 @@ class CommentsController < ApplicationController
 
     def trim_multiple_newlines(comment)
       comment.gsub(/\R{2,}/,"\n")
+    end
+
+    def multiple_lines?(comment)
+      if comment.scan(/\R/).size >= MAX_LINES
+        return true
+      end
+      return false
     end
 end
