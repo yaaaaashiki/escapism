77e826ee58f55cd8efd2460b106833d49a7b4b28
yaaaaashiki <yaaaaakishi@gmail.com>
2017-09-18 10:16:38 +0900

    Add raw sql for joining theses, author and labo table

--- app/controllers/theses_controller.rb
+++ app/controllers/theses_controller.rb
@@ -14,12 +14,15 @@ class ThesesController < ApplicationController
    
     if search_theses?(query, @labo_id, @search_field)
       @theses = Thesis.search_by_keyword(query, @labo_id, @search_field).page(params[:page]).per(4)
-      if not_exist_theses(@theses)
+      @theses_labos_authors = ActiveRecord::Base.connection.select_all("select theses.id, theses.title, theses.body, theses.summary, theses.year, labos.name as labo_name, authors.name as author_name
+                                                                        from theses inner join labos on theses.labo_id = labos.id
+                                                                                    inner join authors on theses.author_id = authors.id
+                                                                       ").to_hash
+      if not_exist_theses(@theses_labos_authors)
         flash[:alert] = 'Matching theses was not found. Try again.'
       end
     end
 
-    @author = Author.all
     @labos = Labo.all
   end
 
--- app/helpers/theses_helper.rb
+++ app/helpers/theses_helper.rb
@@ -2,6 +2,6 @@ module ThesesHelper
   NO_LABO_ID = 9
 
   def display_abst_or_body(thesis)
-    thesis.summary == "" ?  thesis.body.truncate(1000) : thesis.summary
+    thesis["summary"] == "" ?  thesis["body"].truncate(1000) : thesis["summary"]
   end
 end
