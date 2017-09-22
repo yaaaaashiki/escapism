a0744b586b88c853cf59b8c0be3193c9c473f6cb
yaaaaashiki <yaaaaakishi@gmail.com>
2017-09-22 22:53:13 +0900

    Fix labo people table's migration file and update schema.rb

--- db/migrate/20170922134523_labo_people.rb
+++ db/migrate/20170922134523_labo_people.rb
@@ -1,8 +1,10 @@
 class LaboPeople < ActiveRecord::Migration[5.0]
   def change
-    t.references :labo, foreign_key: true
-    t.integer :year
-    t.integer :gender
-    t.integer :number_of_people
+    create_table :labo_people do |t|
+      t.references :labo, foreign_key: true
+      t.integer :year
+      t.integer :gender
+      t.integer :number_of_people
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170912032302) do
+ActiveRecord::Schema.define(version: 20170922134523) do
 
   create_table "admin_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
     t.string   "username",        null: false
@@ -59,6 +59,14 @@ ActiveRecord::Schema.define(version: 20170912032302) do
     t.index ["impressionable_type", "impressionable_id", "session_hash"], name: "poly_session_index", using: :btree
     t.index ["impressionable_type", "message", "impressionable_id"], name: "impressionable_type_message_index", using: :btree
     t.index ["user_id"], name: "index_impressions_on_user_id", using: :btree
+  end
+
+  create_table "labo_people", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+    t.integer "labo_id"
+    t.integer "year"
+    t.integer "gender"
+    t.integer "number_of_people"
+    t.index ["labo_id"], name: "index_labo_people_on_labo_id", using: :btree
   end
 
   create_table "labos", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
@@ -135,6 +143,7 @@ ActiveRecord::Schema.define(version: 20170912032302) do
 
   add_foreign_key "comments", "theses"
   add_foreign_key "comments", "users"
+  add_foreign_key "labo_people", "labos"
   add_foreign_key "messages", "labos"
   add_foreign_key "messages", "users"
   add_foreign_key "theses", "authors"
