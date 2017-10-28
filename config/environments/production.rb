76625ecbabfa9d5b1469673ad28e7d26fd66f0bc
Matsumoto Tatsuki <tatsukimatsumo@gmail.com>
2017-10-28 10:29:43 +0900

    Add mil config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,20 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'hostname' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => ENV['ADRESS'],
+    :port => 587,
+    :domain => ENV['DOMAIN'],
+    :user_name => ENV['USER_NAME'],
+    :password => ENV['PASSWORD'],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
