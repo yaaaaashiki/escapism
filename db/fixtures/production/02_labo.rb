5248f8b8e787d4784aee2c114116c086878b78a7
yaaaaashiki <yaaaaakishi@gmail.com>
2017-11-09 15:51:25 +0900

    Fix prefessor name to uppercase

--- db/fixtures/production/02_labo.rb
+++ db/fixtures/production/02_labo.rb
@@ -21,7 +21,7 @@ end
 ## 空白を含むキーがあるため文字列で指定
 lab_hash = {'鷲見研究室' => 'sumi', 'Dürst 研究室' => 'durst', '佐久田研究室' => 'sakuta',
             '大原研究室' => 'ohara', '小宮山研究室' => 'komiyama', '戸辺研究室' => 'tobe',
-            '原田研究室' => 'harada', 'lopez 研究室' => 'lopez',
+            '原田研究室' => 'harada', 'Lopez 研究室' => 'lopez',
 }
 
 lab_hash.each do |key, value|
