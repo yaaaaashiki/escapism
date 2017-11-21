c1ed31f52946ecd8e0c7c4b308dc08edca98e4fb
Tatsuki Matsumoto <tatsukimatsumo@gmail.com>
2017-11-21 22:43:24 +0900

    Modify error messages in signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,8 +21,6 @@
 require 'date'
 
 class User < ApplicationRecord
-  validate :role_presence
-
   authenticates_with_sorcery!
   has_many :comments
   belongs_to :labos
@@ -31,6 +29,8 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :crypted_password, presence: true
   validates :salt, presence: true
+  validate :role_presence
+  after_validation :remove_unnecessary_error_messages
 
   LABO_STUDENT = 1
   NONE_LABO_STUDENT = 2
@@ -40,15 +40,23 @@ class User < ApplicationRecord
     self.role == LABO_STUDENT ? "研究生" : "学生"
   end
 
-   def labo_student?
-      self.role == LABO_STUDENT
-   end
+  def labo_student?
+    self.role == LABO_STUDENT
+  end
 
-   def belongs_to_this_labo?(room_id)
-     self.labo == room_id
-   end
+  def belongs_to_this_labo?(room_id)
+    self.labo == room_id
+  end
 
-   def role_presence
-    errors[:base] << "研究室への所属有無を選択してください．" unless role.to_i == NONE_LABO_STUDENT || role.to_i == LABO_STUDENT
-   end
+  def role_presence
+    errors[:base] << "研究室への所属有無を選択してください。" unless role.to_i == NONE_LABO_STUDENT || role.to_i == LABO_STUDENT
+  end
+
+  def remove_unnecessary_error_messages
+    if errors.messages.include?(:crypted_password) || errors.messages.include?(:salt)
+      errors.messages.delete(:crypted_password)
+      errors.messages.delete(:salt)
+      errors[:base] << "パスワードを入力してください。"
+    end
+  end
 end
