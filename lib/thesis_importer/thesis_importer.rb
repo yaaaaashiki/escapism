0debec2ffb8d149436be4fd52b5b9762e27fc0d3
Tatsuki Matsumoto <tatsukimatsumo@gmail.com>
2017-11-26 13:29:40 +0900

    Refautor ThesisImporter

--- lib/thesis_importer/thesis_importer.rb
+++ lib/thesis_importer/thesis_importer.rb
@@ -4,51 +4,62 @@ require 'open3'
 module ThesisImporter
   def upsert_all!(theses_year_dir)
     Find.find(theses_year_dir) do |path|
-      if PathChecker.index_html_path?(path)
-        index_html = HTMLParser.parse_html_object(path)
-        written_year = HTMLParser.parse_written_year(index_html)
+      next unless PathChecker.index_html_path?(path)
+      
+      index_html = HTMLParser.parse_html_object(path)
+      written_year = HTMLParser.parse_written_year(index_html)
 
-        index_html.css('tr').each do |tr_element|
-          tr_element.css('td').each do |td_element|
-            td_element.css('a').each do |a_element|
-              if PathChecker.thesis_path?(a_element[:href])
-                begin
-                  thesis = Thesis.new
-                  thesis.year = written_year
+      thesis_base_path = path.gsub(/\/index\.html/, "")
 
-                  thesis.url = "#{path.gsub(/\/index\.html/, "")}/#{a_element[:href]}"
-                  thesis.url = PathChecker.return_martin_labo_full_path(thesis.url) if thesis.belongs_to_martin_labo?
+      index_html.css('tr').each do |tr_element|
+        tr_element.css('td').each do |td_element|
+          td_element.css('a').each do |a_element|
+            next unless PathChecker.thesis_path?(a_element[:href])
+            
+            begin
+              thesis = Thesis.new
+              thesis.year = written_year
+              thesis.url = calculate_url(thesis_base_path, a_element[:href])
+              thesis.labo = Labo.find_by(name: Labo.parse_labo_name(path))
 
-                  thesis.labo = Labo.find_by(name: Labo.parse_labo_name(path))
 
-                  thesis.body = Thesis.extract_body(thesis.url)
 
-                  summariser_name = String(Rails.root.join('lib/abstractor/abstract_creator.py'))
-                  thesis.summary , err, status = Open3.capture3("python3 " + summariser_name + " " + thesis.url)
+              thesis.body = Thesis.extract_body(thesis.url)
 
-                  if thesis.belongs_to_martin_labo?
-                    thesis.title = td_element.content
-                  elsif thesis.belongs_to_harada_labo?
-                    thesis.title = fetch_just_thesis_title(td_element.previous.content)
-                  elsif thesis.belongs_to_sakuta_bachelor_thesis?
-                    thesis.title = fetch_just_thesis_title(td_element.previous.previous.content)
-                  else
-                    thesis.title = fetch_just_thesis_title(td_element.content)
-                  end
+              summariser_name = String(Rails.root.join('lib/abstractor/abstract_creator.py'))
+              thesis.summary , err, status = Open3.capture3("python3 " + summariser_name + " " + thesis.url)
 
-                  author_name = fetch_just_name(tr_element.css('td')[1].content)
-                  author = Author.find_by(name: author_name)
-                  if !author
-                    author = Author.create(name: author_name)
-                  end 
-                  thesis.author = author
 
-                  thesis.save
-                  puts "saved:" + thesis.url
-                rescue
-                  puts "save failed!!:" + thesis.url
-                end
+
+
+
+              if thesis.belongs_to_martin_labo?
+                thesis.title = td_element.content
+              elsif thesis.belongs_to_harada_labo?
+                thesis.title = fetch_just_thesis_title(td_element.previous.content)
+              elsif thesis.belongs_to_sakuta_bachelor_thesis?
+                thesis.title = fetch_just_thesis_title(td_element.previous.previous.content)
+              else
+                thesis.title = fetch_just_thesis_title(td_element.content)
               end
+
+              ActiveRecord::Base.transaction do
+                author_name = fetch_just_name(tr_element.css('td')[1].content)
+                author = Author.find_by(name: author_name)
+                if author.nil?
+                  author = Author.create!(name: author_name)
+                end 
+                thesis.author = author
+
+                thesis.save!
+              end
+
+              puts "saved:" + thesis.url
+              Rails.logger.info("saved:" + thesis.url)
+            rescue => e
+              puts "save failed!!:" + thesis.url
+              Rails.logger.error("save failed!!:" + thesis.url)
+              ErrorUtil.print_error_info(e)
             end
           end
         end
@@ -64,6 +75,16 @@ module ThesisImporter
     string.match(/(:?\r\n)?\s/) ? string.gsub!(/(:?\r\n)?\s/, "") : string
   end
 
+  def calculate_url(base_path, relative_path)
+    url = "#{base_path}/#{relative_path}"
+    url = create_martin_labo_thesis_full_path(url) if url.include?("durst")
+    url
+  end
+
+  def create_martin_labo_thesis_full_path(path)
+    path.gsub(/\.\//, "")
+  end
+
   module_function :upsert_all!
-  module_function :fetch_just_name, :fetch_just_thesis_title
+  module_function :fetch_just_name, :fetch_just_thesis_title, :calculate_url, :create_martin_labo_thesis_full_path
 end
