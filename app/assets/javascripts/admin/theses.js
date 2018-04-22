b99fc5751229d47727b7c5b34f8ece9ad17c6aff
t-matsumo <tatsukimatsumo@gmail.com>
2018-04-22 12:56:10 +0900

    Implement reset forms for change directory ot number of resistration

--- app/assets/javascripts/admin/theses.js
+++ app/assets/javascripts/admin/theses.js
@@ -10,35 +10,41 @@ $(function() {
     }
 
     $('.thesis_form input').val('');
-    $('.thesis_form').hide();
+    $('.thesis_form select').val(0);
+    $('#number_of_registration').val(0);
     $('[class*=theses_others]').hide();
+    $('.thesis_form').hide();
   });
 
   $('#number_of_registration').change(function() {
     let number = $(this).val();
     let forms = $('.thesis_form');
-    
-    if (number === '') {
-      forms.hide();
-    } else {
-      forms.show();
-      $(`.thesis_form:gt(${number - 1})`).hide();
+
+    forms.hide();
+    if (number !== '') {
+      $(`.thesis_form:lt(${number})`).show();
     }
+
+    forms.find('input').val('');
+    forms.find('select').val(0);
+    $('[class*=theses_others]').hide();
   });
   $(`.thesis_form`).hide();
 
   $('[id$=_others_number_of_registration]').change(function() {
-    // 更新対象を探すために$(this)[0]のidからindexを取り出す
+    // 更新対象を探すために$(this)の０番目のidからindexを取り出す
     // idの形式：theses_[index]_others_number_of_registration
-    let targetIndex = $(this)[0].id.slice(7, 8);
+    let targetIndex = $(this).get(0).id.slice(7, 8);
     let forms = $(`.theses_others_${targetIndex}`);
     let number = $(this).val();
-    if (number === '') {
-      forms.hide();
-    } else {
-      forms.show();
-      $(`.theses_others_${targetIndex}:gt(${number - 1})`).hide();
+
+    forms.hide();
+    if (number !== '') {
+      $(`.theses_others_${targetIndex}:lt(${number})`).show();
     }
+
+    forms.find('input').val('');
+    forms.find('select').val(0);
   });
   $('[class*=theses_others]').hide();
 });
