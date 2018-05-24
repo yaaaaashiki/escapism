c84ec73daf3ca318408ab83f4c6ef8aa08e43a4e
t-matsumo <tatsukimatsumo@gmail.com>
2018-05-24 13:24:16 +0000

    Crean assets related AdminLTE2

--- app/assets/javascripts/admin/application.js
+++ app/assets/javascripts/admin/application.js
@@ -1,8 +1,8 @@
-//= require ../AdminLTE/app
-
 //= require jquery
 //= require bootstrap-sprockets
+//= require jquery.slimscroll
+//= require app
 
-//= require jquery_ujs
-//= require common
-//= require footerFixed
+// AdminLTE2-rails関係の変更をしたいときはgeneratorを使うとadmin以外にも影響するのでgeneratorの定義を確認して、手作業で記入www
+// AdminLTE2-railsのプラグインを入れるときは一度コマンドを打ってから関係するファイルすべてを変更
+// assets/javascripts/aplication.jsから追加された行を削除してここに移すだけですむはずwww
