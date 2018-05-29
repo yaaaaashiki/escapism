5717450312a4746028245e3d00eb60843118d49a
yaaaaashiki <yaaaaakishi@gmail.com>
2018-05-29 13:46:38 +0900

    Remove commnets and clean dir of helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,8 +1,2 @@
 module SessionsHelper
-  # sorceryのものをオーバーライドしてたものは消した～
-  # ↓これだけメソッド名がsorceryのものと違うけどremember meを実装したかった？？
-  # def log_in(user)
-  #   session[:user_id] = user.id
-  #   cookies.signed[:user_id] = user.id
-  # end
 end
